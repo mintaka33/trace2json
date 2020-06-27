5cdc7226d190765ce559696772f31a2666322990
mintaka33 <mintaka33@outlook.com>
2020-06-27 18:00:31 -0400

    refine code

--- auto.py
+++ auto.py
@@ -1,11 +1,8 @@
 import os
-import signal
-import subprocess
 import trace2json
-import time
 
-app_cmd = 'ffmpeg -hwaccel vaapi -hwaccel_output_format vaapi -i ~/test.264 -f null -'
-#app_cmd = 'h264encode'
+#app_cmd = 'ffmpeg -hwaccel vaapi -hwaccel_output_format vaapi -i ~/test.264 -f null -'
+app_cmd = 'h264encode'
 
 logfile = 'tmp.log'
 cmd = 'sudo trace-cmd list | grep i915 >' + logfile
@@ -15,33 +12,20 @@ with open(logfile, 'rt') as f:
     lines = f.readlines()
 os.system('rm ' + logfile)
 
-trace_cmd = ''
-trace_cmd += 'sudo trace-cmd start '
+trace_cmd_start = ''
+trace_cmd_start += 'sudo trace-cmd start '
 for l in lines:
-    if l == lines[-1]:
-        c = '-e "' + l.strip() + '"'
-    else:
-        c = '-e "' + l.strip() + '" '
-    trace_cmd += c
-trace_cmdlist = trace_cmd.split(' ') 
-
-with open('trace.sh', 'wt') as f:
-    f.writelines(trace_cmd)
-os.system('chmod +x trace.sh')
+    c = '-e "' + l.strip() + '" '
+    trace_cmd_start += c
 
 os.system('sudo trace-cmd reset')
-os.system(trace_cmd)
-
-#trace_proc = subprocess.Popen(trace_cmdlist, shell=True)
+os.system(trace_cmd_start)
 
 os.system(app_cmd)
 
 os.system('sudo trace-cmd stop')
 os.system('sudo trace-cmd extract -o trace.dat')
 os.system('sudo trace-cmd reset')
-
-#trace_proc.terminate()
-#os.kill(trace_proc.pid, signal.SIGINT)
 
 drm_logfile = 'drm.log'
 trace_report = 'trace-cmd report trace.dat >' + drm_logfile
